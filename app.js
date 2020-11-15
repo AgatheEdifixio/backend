578eb3ee68e37d00f03e14acce6def003682ad04
AgatheEdifixio <agathe.marcellini@edifixio.com>
2020-11-15 08:54:58 +0100

    creation routes POST & USE pour update BDD new obj

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const Thing = require('./models/thing');
 
 const app = express();
 
@@ -20,13 +21,22 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.post('/api/stuff', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: 'Objet créé !'
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
   });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
 });
 
 app.use('/api/stuff', (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+});
+
+/* app.use('/api/stuff', (req, res, next) => {
   const stuff = [
     {
       _id: 'oeihfzeoi',
@@ -46,7 +56,8 @@ app.use('/api/stuff', (req, res, next) => {
     },
   ];
   res.status(200).json(stuff);
-});
+}); */
+
 
 module.exports = app;
 
