5f6070d2af924d97b4565ed3469dbd4277d31fbc
AgatheEdifixio <agathe.marcellini@edifixio.com>
2020-11-15 11:37:51 +0100

    maj

--- app.js
+++ app.js
@@ -30,6 +30,12 @@ app.post('/api/stuff', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 });
 
+app.get('/api/stuff', (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+});
+
 app.get('/api/stuff/:id', (req, res, next) => {
   Thing.findOne({ _id: req.params.id })
     .then(thing => res.status(200).json(thing))
@@ -49,11 +55,7 @@ app.delete('/api/stuff/:id', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 });
 
-app.use('/api/stuff', (req, res, next) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-});
+
 
 module.exports = app;
 
